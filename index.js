8fcd602632ff1ca873f426d9714d49c436ad6842
MDShakibul <sakib6610@gmail.com.com>
2022-05-24 22:15:48 +0600

    all data show

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port =process.env.Port || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhql6d1.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect();
+        const productsCollection = client.db('good_deal_motors').collection('products');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+          });
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req,res)=>{
     res.send("Hello World")
